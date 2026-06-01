f85ff4cb90ba4536e95309e049525b3d6d338434
atharvaajoshii <jatharva1701@gmail.com>
2026-06-01 23:44:41 +0530

    completed backend initialisation and connection with database

---
+++ EventWave/backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const db = require("./config/db");
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("backend running properly");
+});
+db.getConnection()
+    .then((connection)=>{
+        console.log("databse connected ");
+        connection.release();
+    })
+    .catch((err)=>{
+        console.log("database not connected:",err.message);
+    })
+
+
+
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`);
+});
+
