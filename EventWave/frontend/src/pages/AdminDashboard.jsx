cd0512a3ed942252dbbe1f537dcaa0130b508292
atharvaajoshii <jatharva1701@gmail.com>
2026-06-03 16:20:48 +0530

    admindashboard inistialisation

--- EventWave/frontend/src/pages/AdminDashboard.jsx
+++ EventWave/frontend/src/pages/AdminDashboard.jsx
@@ -1,13 +1,48 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function AdminDashboard() {
 
-    const role = localStorage.getItem("role");
+    const [events, setEvents] = useState([]);
 
-    return (
-        <div>
-            <h1>Admin Dashboard</h1>
-            <p>Logged in as {role}</p>
-        </div>
-    );
+    const [eventname, setEventname] = useState("");
+    const [date, setDate] = useState("");
+    const [venue, setVenue] = useState("");
+    const [capacity, setCapacity] = useState("");
+
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        fetchEvents();
+    }, []);
+
+    const fetchEvents = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            const response = await api.get(
+                "/events",
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+            setEvents(response.data);
+        } catch (error) {
+            console.log(error);
+            setError("Failed to load events");
+        }
+    };
+
+return (
+    <div>
+        <h1>Admin Dashboard</h1>
+
+        <p>Total Events: {events.length}</p>
+
+        {error && <p>{error}</p>}
+    </div>
+);
 }
 
 export default AdminDashboard;
